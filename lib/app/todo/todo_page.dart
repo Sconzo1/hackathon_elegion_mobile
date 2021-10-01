3fa1b4b66daebf460f59fadb5e483a26160f6546
Belig Chimitov <belig.ch111@gmail.com>
2021-10-02 02:19:30 +0700

    added introduction page

--- lib/app/todo/todo_page.dart
+++ lib/app/todo/todo_page.dart
@@ -5,6 +5,8 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text("TODO LIST"),
+    );
   }
 }
