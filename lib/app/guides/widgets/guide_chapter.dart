4060f628195cd9b04ffd3049a6da9edc32bbdec9
Aleksey <kudashkin_aleksey@mail.ru>
2021-10-02 14:28:43 +0700

    third successful merge in a row!! pdf view fix

--- lib/app/guides/widgets/guide_chapter.dart
+++ lib/app/guides/widgets/guide_chapter.dart
@@ -1,46 +1,39 @@
+import 'package:e_legion_hackaton/app/guides/widgets/pdf_viewer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:native_pdf_view/native_pdf_view.dart';
 
 class GuideChapter extends StatelessWidget {
   final MapEntry<String, List<Chapter>> grouped_chapters;
-  PdfController _pdfController = PdfController(
-    document: PdfDocument.openAsset('assets/sample.pdf'));
 
   GuideChapter({Key? key, required this.grouped_chapters}) : super(key: key);
-  // Widget pdfView() => PdfView(
-  //   controller: pdfController,
-  // );
-
-
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Column(
-          children: [
-            ListTile(
-                title: Text(grouped_chapters.key,
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400))
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: [
+          ListTile(
+            onTap: () => {
+              Navigator.push(context, MaterialPageRoute(builder: (_) => MyPdfViewer()))
+            },
+            title: Text(
+              grouped_chapters.key,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
             ),
-
-            for (var subChapter in this.grouped_chapters.value)
-              ListTile(
-                  leading: Icon(Icons.document_scanner),
-                  title: Text(subChapter.description)
-              ),
-          ],
-        ),
+          ),
+          for (var subChapter in this.grouped_chapters.value)
+            ListTile(
+              onTap: () => {
+                Navigator.push(context, MaterialPageRoute(builder: (_) => MyPdfViewer()))
+              },
+              leading: Icon(Icons.document_scanner),
+              title: Text(subChapter.description),
+            ),
+        ],
       ),
-      onTap: () => {
-      _pdfController.loadDocument(
-      PdfDocument.openAsset('assets/dummy.pdf'))
-
-      },
     );
   }
 }
